9b95a7f1e141cfe216ed77b74e9ae3f7016a804c
Lucy M Golcher <luciamongeg@gmail.com>
2019-10-31 20:33:10 -0600

    Avance en variables.

--- MayCEy.pl
+++ MayCEy.pl
@@ -49,15 +49,19 @@ condición("Distancia").
 
 % Analisis de la gramática
 
-oracion_inicial(X,Y):- saludo(X,Y).
+oracion_inicial(X,Y):- saludo(X,Y), write("Hola, por favor identifiquese").
 saludo --> sin_nom.
 sin_nom--> com1,sust. %Hola/hola MayCEy
 sin_nom--> com2,com3,sust. %Buenos/buenos dias/tardes/noches MayCEy
 sin_nom--> com2,com3. %Buenos/buenos dias/tardes/noches
+sin_nom--> verb,prep,sust.
+
 sust--> [W],{sustantivo(W)}.
 com1--> [W],{comple1(W)}.
 com2--> [W],{comple2(W)}.
 com3--> [W],{comple3(W)}.
+verb--> [W],{verbo(W)}.
+prep--> [W],{prepos(W)}.
 
 %Base de datos para el saludo
 sustantivo("MayCEy").
@@ -68,13 +72,21 @@ comple2("buenos").
 comple3("dias").
 comple3("tardes").
 comple3("noches").
+verbo("llamando").
+prepos("a").
 
+oracion_inicial(X,Y):- emergencia(X,Y), write("Cual es su emergencia?").
+emergencia --> sin_nom.
+sin_nom--> help.  %Mayday
+sin_nom--> help,help. %Mayday Mayday
+help --> [W],{id(W)}.
+id("Mayday").
+id("mayday").
 
 iniciar_MayCEy:-
     read(X), %IMPORTANTE responder entre comillas
     split_string(X," ","",L),
-    oracion_inicial(L, []),
-    write("Hola, por favor identifiquese").
+    oracion_inicial(L, []).
    %se hace otro read
    %se ejecuta otra funcion que siga respondiendo, una que se llame darpista o similar, que sea un ciclo
 
